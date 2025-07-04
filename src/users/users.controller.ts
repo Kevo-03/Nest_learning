de387a08db34517214d06bff2b2bd4a198fc893f
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-04 14:17:51 +0300

    users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete, Query, Param, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,5 +11,25 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.service.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.service.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.service.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.service.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.service.update(parseInt(id), body);
     }
 }
